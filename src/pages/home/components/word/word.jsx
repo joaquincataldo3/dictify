587ccd70032e66a27d581fb99f3efb9af34866bd
joaquincataldo3 <joaco.cataldo3@gmail.com>
2023-03-11 11:13:40 -0500

    audio play fn

--- src/pages/home/components/word/word.jsx
+++ src/pages/home/components/word/word.jsx
@@ -1,29 +1,45 @@
 import { useGlobalContext } from "../../../../hooks/context"
+import {Howl, Howler} from 'howler'
+import './word.css'
 
 function Word() {
 
   const { data } = useGlobalContext()
 
-  return (
+  const playSound = (src) => {
+    const sound = new Howl({src})
+    sound.play()
+  }
 
-    <div className="word-content-container">
+  Howler.volume(1.0)
+
+  return (
+    <>
       {
         data.length > 0 &&
-        <div className="word-container">
+        <div className="word-content-container">
+          <div className="word-container">
 
-          {data.map(data => {
-            const { word } = data
-            return <h2>{word}</h2>
-          })}
+            {data.map((data, i) => {
+              const { word } = data
+              return <h2 key={i} className="word">{word}</h2>
+            })}
 
-          {data.map(data => {
-            const {text} = data.phonetics[1]
-           return <h4>{text}</h4> 
-          })}
+            {data.map((data, i) => {
+              const { text } = data.phonetics[1]
+              return <h4 key={i} className="pronunciation">{text}</h4>
+            })}
+          </div>
+          <div className="word-audio-container">
+          {data.map((data, i) => {
+              const { audio } = data.phonetics[0]
+              return <i key={i} className='bx bx-play-circle' onClick={() => playSound(audio)}></i>
+            })}
+          
+          </div>
         </div>
       }
-    </div>
-
+    </>
   )
 }
 
